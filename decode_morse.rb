09d3c443fb464e1e597859f8b847a04d932e3e56
Esmatullah Ahmadzai (Esmat) <ea.ahmadzai2020@gmail.com>
2022-09-28 20:24:25 +0430

    Add method to decode characters

--- decode_morse.rb
+++ decode_morse.rb
@@ -9,4 +9,6 @@ def decode_character(letter)
   def decode_word(word)
     word.split(/ /).map { |letter| decode_character letter }.join
   end
-  
+  def decode_sentence(sentence) sentence.split(/ /).map { |word| decode_word word }.join(' ') 
+  end
+puts(decode_sentence(".- -... --- -..- ..-. ..- .-.. .-.. --- ..-. .-. ..- -... .. . ...")) 
