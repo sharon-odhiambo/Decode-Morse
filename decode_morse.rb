b9784bf3924f03e9d1e9942d061844b96e31ae7e
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-09-28 19:05:30 +0300

    fix linter errors

--- decode_morse.rb
+++ decode_morse.rb
@@ -1,14 +1,16 @@
 def decode_character(letter)
-    dictionary = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
-                  '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
-                  '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
-                  '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z' }
-    dictionary[letter]
-  end
-  
-  def decode_word(word)
-    word.split(/ /).map { |letter| decode_character letter }.join
-  end
-  def decode_sentence(sentence) sentence.split(/ /).map { |word| decode_word word }.join(' ') 
-  end
-puts(decode_sentence(".- -... --- -..- ..-. ..- .-.. .-.. --- ..-. .-. ..- -... .. . ...")) 
+  dictionary = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
+                 '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
+                 '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
+                 '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z' }
+  dictionary[letter]
+end
+
+def decode_word(word)
+  word.split(/ /).map { |letter| decode_character letter }.join
+end
+
+def decode_sentence(sentence)
+  sentence.split(/ /).map { |word| decode_word word }.join(' ')
+end
+puts(decode_sentence('.- -... --- -..- ..-. ..- .-.. .-.. --- ..-. .-. ..- -... .. . ...'))
